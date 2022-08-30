c9fcb02f3553bb92cc9145aa6577d5c100af7cc2
Safkat khan Tafsir <safkatkhan420@gmail.com>
2022-08-30 22:21:12 +0600

    partice js

--- Hello Javascript/First.js
+++ Hello Javascript/First.js
@@ -1,7 +1,89 @@
-var name = "safkat";
-var name1 ="ami";
+var nam1 = "safkat";
+
+ var onoionPrice = 80; 
+ var eggprice = 50;
 
 
-console.log(name);
-// 
-// 
+ var totalPrice = onoionPrice+eggprice;
+ var priceDifferencr = onoionPrice-eggprice;
+ var negative = eggprice-onoionPrice;
+
+ var monthly = 10000;
+ var month = 12;
+ var salaryonYear = monthly*month;
+
+ var have = 1000;
+ var player =10;
+ var share = have/player;
+
+ var price = '120';
+ console.log(price);
+ var totalPrice=parseInt(price);
+
+  console.log(totalPrice);
+  console.log(nam);
+
+ var gpa = '3.14'
+   console.log(gpa);
+ var gpaNumber = parseFloat(gpa);
+  console.log(gpaNumber);
+
+var surname = 'khan';
+var fullName = nam+' '+surname;
+ console.log(fullName);
+
+
+ console.log(typeof totalPrice);
+ console.log(typeof fullName);
+
+var number = 0.1115555;
+number= number.toFixed(2);
+number=parseFloat(number);
+
+console.log(number);
+
+
+
+
+
+ console.log(totalPrice);
+ console.log(priceDifferencr);
+ console.log(negative);
+ console.log(salaryonYear);
+ console.log(share);
+
+
+
+
+var roll = 22463921;
+var cgpa = 3.1416283;
+var yourCgpa = '7.5556454';
+var nam = 'safkat';
+var surname = 'khan';
+var fullName = nam +" "+ surname;
+
+console.log(fullName);
+
+console.log(nam);
+
+var nam = (typeof nam);
+
+var yourCgpa = parseFloat(yourCgpa);
+cgpa = cgpa.toFixed(2);
+cgpa = parseFloat (cgpa);
+console.log(cgpa);
+
+
+console.log(yourCgpa);
+
+var cgpa = (typeof cgpa);
+
+console.log(nam);
+console.log(cgpa);
+
+// --------------Write down the out put----------------------
+
+
+
+
+
---
+++ Hello Javascript/partise.js
@@ -0,0 +1,17 @@
+const person = [
+    {
+      name: "rahim",
+      age: 22,
+      friends: ["rahim,karim,jabbar"],
+    },
+    {
+      name: "rahim2",
+      age: 22,
+      friends: ["rahim,karim,jabbar"],
+    },
+    {
+    name: "rahim3",
+    age: 22,
+    friends: ["rahim,karim,jabbar"],
+  },
+  ];
